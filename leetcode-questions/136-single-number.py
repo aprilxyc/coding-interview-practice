03a1204f9b053829a3df9769a96b1bb87548a080
aprilxyc <aprilchi4@gmail.com>
2020-01-05 02:06:58 +1100

    added 136 xor

--- leetcode-questions/136-single-number.py
+++ leetcode-questions/136-single-number.py
@@ -1,9 +1,18 @@
 https://leetcode.com/problems/single-number/discuss/468420/Python3-Bitwise-and-Non-Bitwise-Solutions-(w-Explanation)
 
-# study this
+# study this using bitwise operators (the intended solution)
 def singleNumber(self, nums: List[int]) -> int:
-	sol = 0
+	result = 0
 	for n in nums:
-		sol ^= n
+		result ^= n
 
-	return sol
+	return result
+
+
+# double the sum of the set and remove the sum of the original array
+def singleNumber(self, nums: List[int]) -> int:
+	# [1, 1, 2, 2, 3] -> [1, 2, 3]
+	return 2 * sum(set(nums)) - sum(nums)
+
+# can also do this using a dictionary (not shown)
+
