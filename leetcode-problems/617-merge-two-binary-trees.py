3d7ec221ff610d42cf18bb2e1130172b55072ac1
aprilxyc <aprilchi4@gmail.com>
2020-03-01 22:54:27 +1100

    redid tree q

--- leetcode-problems/617-merge-two-binary-trees.py
+++ leetcode-problems/617-merge-two-binary-trees.py
@@ -80,12 +80,24 @@ class Solution:
         t1.right = self.mergeTrees(t1.right, t2.right) # save it into t1
         return t1
             
-            
+# relook at this
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         
+        root_node = None
         
-            
-            
-            
-            
+        if not t1:
+			# t1 is empty, new tree is decided by t2
+            return t2
         
+        elif not t2:
+			# t2 is empty, new tree is decided by t1
+            return t1
         
+        else:
+            # both t1 and t2 exist, merge current node, and traverse on DFS again
+            root_node =  TreeNode( t1.val + t2.val )
+            root_node.left = self.mergeTrees( t1.left, t2.left )
+            root_node.right = self.mergeTrees( t1.right, t2.right )
+            
+            return root_node
